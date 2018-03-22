a6c5302ae3171ed8c0b2de3ecb376441ad906eba
mbossa49 <mbossa49@gmail.com>
2018-03-23 00:03:23 +0100

    clean

--- src/main/java/fr/cnam/SpringBootWebApplication.java
+++ src/main/java/fr/cnam/SpringBootWebApplication.java
@@ -9,23 +9,4 @@ public class SpringBootWebApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
-
-    //Tomcat large file upload connection reset
-    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
-    /*@Bean
-    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
-
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-
-        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
-            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
-                //-1 means unlimited
-                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
-            }
-        });
-
-        return tomcat;
-
-    }*/
-
 }
