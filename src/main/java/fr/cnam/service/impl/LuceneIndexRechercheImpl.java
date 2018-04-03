c1fd226ca9b138609a1ec3eeeeefb040be17417a
mbossa49 <mbossa49@gmail.com>
2018-04-03 10:26:02 +0200

    suppr import

--- src/main/java/fr/cnam/service/impl/LuceneIndexRechercheImpl.java
+++ src/main/java/fr/cnam/service/impl/LuceneIndexRechercheImpl.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
 import java.text.ParseException;
