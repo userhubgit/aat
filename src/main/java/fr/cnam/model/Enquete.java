eb08357a5f14ee300fb4c1e44b54d9c555c218aa
mbossa49 <mbossa49@gmail.com>
2018-04-03 14:28:43 +0200

    test BDD integ

--- src/main/java/fr/cnam/model/Enquete.java
+++ src/main/java/fr/cnam/model/Enquete.java
@@ -1,24 +1,24 @@
-//package fr.cnam.model;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//@Entity
-//public class Enquete {
-//
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
-//	private long id;
-//	
-//	private String userInput;
-//
-//	public String getUserInput() {
-//		return userInput;
-//	}
-//
-//	public void setUserInput(String userInput) {
-//		this.userInput = userInput;
-//	}
-//}
+package fr.cnam.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Enquete {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String userInput;
+
+	public String getUserInput() {
+		return userInput;
+	}
+
+	public void setUserInput(String userInput) {
+		this.userInput = userInput;
+	}
+}
